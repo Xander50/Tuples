1220025d692bb7f041c80b2f52e3fdddaf75f53e
Xander50 <73202723+Xander50@users.noreply.github.com>
2025-01-18 12:26:11 -0500

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,51 @@
+# tup = ()
+# print(type(tup))
+
+# tuple1 = (9)
+# print(type(tuple1))
+
+# tuple1 = (9,)
+# print(type(tuple1))
+
+# tuple2 = ('a','e','i','o','u')
+# print(tuple2, type(tuple2))
+
+# tuple3 = (4,5,'l',7,'k','p')
+# print(tuple3,type(tuple3))
+
+# tuple4 = 'g',5,2,'t','o'
+# print(tuple4,type(tuple4))
+
+# tuple4 = 'g',5,2,'t','o'
+# print(tuple4[2])
+# print(tuple4[-2])
+# print(tuple4[1:4])
+# print(tuple4[2:])
+# print(tuple4[:3])
+# print(tuple4[:-2])
+
+# tuple5 = 5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+# print(tuple5[6][1])
+
+# tuple5 = 5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+# print(tuple5[10])
+
+# tuple5 =5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+#tuple5[1] = 'a'
+# del tuple5[5]
+# tuple5.append('v')
+# print(tuple5)
+
+# tuple5 = 5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+# del tuple5
+# print(tuple5)
+
+# tuple5 = 5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+# tuple5[6].append('k')
+# print(tuple5)
+
+tuple5 = 5,'u',8,'r',3,5,[6,'h'],['0',0],2,'4'
+# print(tuple5.index('u'))
+# print(tuple5.index(10))
+print(tuple5.count(8))
+print(tuple5.count(10))
